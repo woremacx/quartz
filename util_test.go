361dbd53df7aecebb149f2cc66e41ad4a16bd770
woremacx <woremacx@gmail.com>
2021-04-15 09:30:30 +0900

    Change to use local timezone

--- util_test.go
+++ util_test.go
@@ -9,19 +9,19 @@ import (
 
 func assertEqual(t *testing.T, a interface{}, b interface{}) {
 	if !reflect.DeepEqual(a, b) {
-		t.Fatalf("%v != %v", a, b)
+		t.Fatalf("actual(%v) != expected(%v)", a, b)
 	}
 }
 
 func assertEqualInt64(t *testing.T, a int64, b int64) {
 	if a != b {
-		t.Fatalf("%d != %d", a, b)
+		t.Fatalf("actual(%d) != expected(%d)", a, b)
 	}
 }
 
 func assertNotEqual(t *testing.T, a interface{}, b interface{}) {
 	if reflect.DeepEqual(a, b) {
-		t.Fatalf("%v == %v", a, b)
+		t.Fatalf("actual(%v) == expected(%v)", a, b)
 	}
 }
 
